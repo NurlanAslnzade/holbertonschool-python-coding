1aee5ed46d8c013ec3c73ab72d5484b74aa32d02
dasda <dasd>
2025-12-08 14:15:54 +0000

    s

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,12 +10,13 @@ class Square:
             raise TypeError("size must be an integer")  
         if size < 0:
             raise ValueError("size must be >= 0")
+    
     def area(self):
         return self.__size * self.__size
         
     @property
     def size(self):
-        if not isinstance(size, int):
+        if not isinstance(self.__size, int):
             raise Exception("size must be an integer")  
         if self.__size < 0:
             raise Exception("size must be >= 0")

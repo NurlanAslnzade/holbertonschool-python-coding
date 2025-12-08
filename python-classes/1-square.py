971bb58a2b3ce58d13c45009916bf6a2065efad9
dasda <dasd>
2025-12-08 13:53:08 +0000

    s

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a Square class"""
+
+
+class Square:
+    """This module defines a Square class"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be >= 0")  
+        if size < 0:
+            raise ValueError("size must be an integer")
